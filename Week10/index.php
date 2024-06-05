2a9deb6955ac641e93c65281702680707ce2b96b
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-06-05 07:27:36 +0700

    Kelas Web Programming Week 10 dan 11

---
+++ Week10/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 10 Web Programming</title>
+    <style>
+        div{
+            background-color: yellow;
+            width: auto;
+            height: 100px;
+            text-align: center;
+
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div>Block 1</div>
+    <div>Block 2</div>
+    <p>Par</p>
+</body>
+</html>
---
+++ Week11/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 11</title>
+    <style type="text/css">
+        #container {
+            background-color: grey;
+        }
+
+        #div1 {
+            width: 100px;
+            height: 200px;
+            background-color: blue;
+        }
+
+        #div2 {
+            width: 100px;
+            height: 200px;
+            background-color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="container">
+        <div id="div1">
+
+        </div>
+        <div id="div2">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
